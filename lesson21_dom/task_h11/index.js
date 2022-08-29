28f1449a1a66c9cb760e96a26de54a08cdff0592
karrmao <karrmao@gmail.com>
2022-08-29 23:19:35 +0300

    add

---
+++ lesson21_dom/task_h11/index.js
@@ -0,0 +1,9 @@
+export const squaredNumbers = () => {
+  const listItems = [...document.querySelectorAll('.number')];
+
+  listItems.map((num) => {
+    const squered = num.dataset.number * num.dataset.number;
+    num.dataset.squaredNumber = squered;
+  });
+};
+//squaredNumbers();

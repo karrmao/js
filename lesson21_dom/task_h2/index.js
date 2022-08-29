3494d188b3be5e59c311292a62f34739fa2d3fe0
karrmao <karrmao@gmail.com>
2022-08-29 15:48:08 +0300

    change

--- lesson21_dom/task_h2/index.js
+++ lesson21_dom/task_h2/index.js
@@ -6,28 +6,23 @@ export const getItemsList = () => {
 
 getItemsList();
 
-export const getItemsArray = () => {
-  const nodeElementList = document.querySelectorAll('.tool');
-  const elementsArray = Array.from(nodeElementList);
-  console.dir(elementsArray);
-  return elementsArray;
-};
-
-getItemsArray();
-
-// export const getItemsList = () => {
-//   const elementsList = document.querySelectorAll('.technology');
-//   console.dir(elementsList);
-//   return elementsList;
-// };
-
-// getItemsList();
-
-// export const getItemsArray = () => {
-//   const elementsArray = document.querySelectorAll('.tool');
-//   const NodeList = Array.from(elementsArray);
-//   console.dir(NodeList);
-//   return NodeList;
+// /*export*/ const getItemsArray = () => {
+//
 // };
 
 // getItemsArray();
+
+export const getItemsArray = () => {
+  //const nodeElementList = document.querySelectorAll('.tool');
+  //   const elementsArray = Array.from(nodeElementList);
+  //   console.dir(elementsArray);
+  //   return elementsArray;
+
+  //refactoring
+  const nodeElementList = Array.from(document.querySelectorAll('.tool'));
+  console.dir(nodeElementList);
+  return nodeElementList;
+};
+
+getItemsArray();
+/*export*/

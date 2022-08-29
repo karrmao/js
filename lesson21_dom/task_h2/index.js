2dee251eacb0892234a8165062cc2a3750faec58
karrmao <karrmao@gmail.com>
2022-08-29 15:44:36 +0300

    change

--- lesson21_dom/task_h2/index.js
+++ lesson21_dom/task_h2/index.js
@@ -1,12 +1,12 @@
-const getItemList = () => {
+export const getItemsList = () => {
   const elementList = document.querySelectorAll('.technology');
   console.dir(elementList);
   return elementList;
 };
 
-getItemList();
+getItemsList();
 
-const getItemsArray = () => {
+export const getItemsArray = () => {
   const nodeElementList = document.querySelectorAll('.tool');
   const elementsArray = Array.from(nodeElementList);
   console.dir(elementsArray);
@@ -14,3 +14,20 @@ const getItemsArray = () => {
 };
 
 getItemsArray();
+
+// export const getItemsList = () => {
+//   const elementsList = document.querySelectorAll('.technology');
+//   console.dir(elementsList);
+//   return elementsList;
+// };
+
+// getItemsList();
+
+// export const getItemsArray = () => {
+//   const elementsArray = document.querySelectorAll('.tool');
+//   const NodeList = Array.from(elementsArray);
+//   console.dir(NodeList);
+//   return NodeList;
+// };
+
+// getItemsArray();

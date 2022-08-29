c1775a61f7b2d167f2c35fcf3ce570122f03c0c9
karrmao <karrmao@gmail.com>
2022-08-29 20:29:44 +0300

    add

--- lesson21_dom/task_h7/main.js
+++ lesson21_dom/task_h7/main.js
@@ -1,9 +1,28 @@
+// export const createButton = (buttonText) => {
+//   const addButton = document.createElement('button');
+//   addButton.textContent = buttonText;
+
+//   const bodyElem = document.querySelector('body');
+//   bodyElem.append(createButton);
+// };
+
 export const createButton = (buttonText) => {
   const addButton = document.createElement('button');
   addButton.textContent = buttonText;
 
-  const bodyElem = document.querySelector('body');
-  bodyElem.append(createButton);
+  document.querySelector('body').append(createButton);
 };
 
 //createButton('Кнопочка');
+
+// export const createButton = buttonText => {
+//   //document.createElement('button');
+//   const button = document.createElement('button');
+//   console.log(button);
+//   button.textContent = buttonText;
+//   document.querySelector('body').append(button);
+//   //return button;
+// };
+// };
+/*export*/
+//console.log(createButton('Просто кнопка'));

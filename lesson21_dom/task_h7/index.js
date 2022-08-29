351005c75aa09fabd1ad8a2cf636081bfc294ce4
karrmao <karrmao@gmail.com>
2022-08-29 20:55:28 +0300

    add

---
+++ lesson21_dom/task_h7/index.js
@@ -0,0 +1,16 @@
+// const createButton1 = (buttonText) => {
+//   const addButton = document.createElement('button');
+//   addButton.textContent = buttonText;
+
+//   const bodyElem = document.querySelector('body');
+//   bodyElem.append(addButton);
+// };
+// createButton1('Button1');
+
+export const createButton = (buttonText) => {
+  const addButton = document.createElement('button');
+  addButton.textContent = `${buttonText}`;
+  document.querySelector('body').append(addButton);
+};
+
+//createButton('Button');

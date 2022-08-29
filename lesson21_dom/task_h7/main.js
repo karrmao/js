39e5265a4c8dd4647312a2ebce0a8673f12dd874
karrmao <karrmao@gmail.com>
2022-08-29 20:25:32 +0300

    add

--- lesson21_dom/task_h7/main.js
+++ lesson21_dom/task_h7/main.js
@@ -1,6 +1,7 @@
 export const createButton = (buttonText) => {
-  const createButton = document.createElement('button');
-  createButton.textContent = buttonText;
+  const addButton = document.createElement('button');
+  addButton.textContent = buttonText;
+
   const bodyElem = document.querySelector('body');
   bodyElem.append(createButton);
 };

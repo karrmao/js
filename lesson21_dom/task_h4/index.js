4457ba81863066c3dd8accb8fa8399c88779caf1
karrmao <karrmao@gmail.com>
2022-08-29 16:47:01 +0300

    add

--- lesson21_dom/task_h4/index.js
+++ lesson21_dom/task_h4/index.js
@@ -1,12 +1,6 @@
-// const setTitle = (text) => {
-//   const x = (document.querySelector('.title').textContent = text);
-//   console.dir(x);
-// };
-
-// setTitle('Hi, whats up?');
-
 export const setTitle = (text) => {
-  console.dir((document.querySelector('.title').textContent = text));
+  document.querySelector('.title').textContent = text;
 };
 
-setTitle('Hi, whats up?');
+//setTitle('Hi, whats up?');
+//=> Hi, whats up?

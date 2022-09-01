4185d7bd7c2dd5780f1a1223860bd33257e2dbf4
karrmao <karrmao@gmail.com>
2022-09-01 16:08:30 +0300

    add

--- lesson_24_dates/task1/index.js
+++ lesson_24_dates/task1/index.js
@@ -1,11 +1,28 @@
-export const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+// const dayOfWeek = (date, days) => {
+//   const day = new Date(date).getDate();
+//   //console.log(day);
+//   const dayInFuture = new Date(date).setDate(day + days);
+//   // console.log(dayInFuture);
+//   return dayInFuture;
+// };
+// /*************************************************/
+// /*[Jan0],[Feb1],[Mar2],[Apr3],[May4],[Jun5]
+// /*[Jly6],[Aug7],[Sep8],[Oct9],[Nov10],[Dec11]
+// /*************************************************/
+// const result = dayOfWeek(new Date(2022, 8, 1), 5);//2022/[Sep8]/1 + 5
+// console.log(new Date(result));
+// //=> Wed Dec 07 2022 00:00:00 GMT+0200
+//
+/*************************************************/
+//T1
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dayInFuture = new Date(date).setDate(day + days);
 
   return weekDays[new Date(dayInFuture).getDay()];
 };
 
-const result = dayOfWeek(new Date(2019, 0, 1), 14);
-console.log(result);
+const result = dayOfWeek(new Date(2022, 9, 1), 5);
+console.log(result); //=> Th

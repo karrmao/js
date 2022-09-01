e127bd152f246e6e0beb4d95899732823d0f9d6d
karrmao <karrmao@gmail.com>
2022-09-01 16:32:37 +0300

    add

--- lesson_24_dates/task2/index.js
+++ lesson_24_dates/task2/index.js
@@ -1,10 +1,12 @@
-export const formatter = new Intl.DateTimeFormat('en', {
+export const getGreenwichTime = (date) => {
+  return formatter.format(date);
+};
+
+const formatter = new Intl.DateTimeFormat('en', {
   timeZone: 'UTC',
   hour: '2-digit',
   minute: '2-digit',
   hour12: false,
 });
-
-const getGreenwichTime = (date) => formatter.format(date);
 
 console.log(getGreenwichTime(new Date()));

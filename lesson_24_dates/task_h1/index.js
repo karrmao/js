9a40b8c408d8f844ef9f2789ae79d12c095dab78
karrmao <karrmao@gmail.com>
2022-09-01 18:20:46 +0300

    add

--- lesson_24_dates/task_h1/index.js
+++ lesson_24_dates/task_h1/index.js
@@ -1,6 +1,5 @@
 export const getDiff = (startDate, endDate) => {
   const difTime = Math.abs(startDate - endDate);
-  //console.log(difTime);
 
   const days = Math.floor(difTime / (1000 * 60 * 60 * 24));
   const hours = Math.floor(difTime / (1000 * 60 * 60)) - days * 24;
@@ -17,5 +16,6 @@ export const getDiff = (startDate, endDate) => {
 
 const firstDate = new Date(2022, 8, 5, 18, 00);
 const lastDate = new Date(2022, 8, 3, 17, 00);
+
 console.log(getDiff(firstDate, lastDate));
 //=>  2d 1h 0m 0s

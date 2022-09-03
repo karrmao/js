e547e72f8b0fa7f20a09d5c607e0bff54c427e0a
karrmao <karrmao@gmail.com>
2022-09-03 15:32:53 +0300

    d

--- lesson_28_func_advamced/task1/index.js
+++ lesson_28_func_advamced/task1/index.js
@@ -37,11 +37,11 @@ export const calc = (initialValue) => {
   return calculator;
 };
 
-const resultFirst = calc(3).add(2).mult(4).div(10).subtract(5).result();
-console.log(resultFirst); //=> -3
+// const resultFirst = calc(3).add(2).mult(4).div(10).subtract(5).result();
+// console.log(resultFirst); //=> -3
 
-const resultSecond = calc(100).add(500).mult(2).div(1000).subtract(10).result();
-console.log(resultSecond); //=> -8.8
+// const resultSecond = calc(100).add(500).mult(2).div(1000).subtract(10).result();
+// console.log(resultSecond); //=> -8.8
 
-const resultThird = calc(10).add(50).mult(2).div(10).subtract(4).result();
-console.log(resultThird); //=> 8
+// const resultThird = calc(10).add(50).mult(2).div(10).subtract(4).result();
+// console.log(resultThird); //=> 8

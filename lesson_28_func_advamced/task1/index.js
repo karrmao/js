f8078a6404edc51ffd5571f8933121dc52a78fb1
karrmao <karrmao@gmail.com>
2022-09-03 15:29:13 +0300

    fixed

--- lesson_28_func_advamced/task1/index.js
+++ lesson_28_func_advamced/task1/index.js
@@ -1,38 +1,41 @@
 //calc(3).add(2).mult(4).div(10).subtract(5).result();
 //3+2*4/10-5 = -3
 
-export const calc = (initValue) => {
-  let result = initValue;
+export const calc = (initialValue) => {
+  let result = initialValue;
 
   const calculator = {
     add(number) {
       result += number;
-      //return this or
-      return calculator;
-    },
-
-    substract(number) {
-      result -= number;
+      //return calculator;
       return this;
     },
 
     mult(number) {
       result *= number;
+      //return calculator;
+      return this;
+    },
+
+    subtract(number) {
+      result -= number;
+      //return calculator;
       return this;
     },
 
     div(number) {
       result /= number;
+      //return calculator;
       return this;
     },
 
     result() {
+      //return calculator;
       return result;
     },
   };
-
   return calculator;
 };
 
-const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
 console.log(result);

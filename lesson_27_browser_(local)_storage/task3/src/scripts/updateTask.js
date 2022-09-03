2c5da80d9e98f91b1e3590d72b1d8aeee098ee3e
karrmao <karrmao@gmail.com>
2022-09-03 14:21:20 +0300

    add

--- lesson_27_browser_(local)_storage/task3/src/scripts/updateTask.js
+++ lesson_27_browser_(local)_storage/task3/src/scripts/updateTask.js
@@ -19,3 +19,26 @@ export const onToggleTask = () => {
 
   renderTasks();
 };
+
+////
+export const onToggleTask = (e) => {
+  const isCheckBox = e.target.classList.contains('list__item-checkbox');
+
+  if (!isCheckBox) {
+    return;
+  }
+  const taskslist = getItem('tasksList');
+  const newTasksList = taskslist.map((task) => {
+    if (task.id === e.target.dataset.id) {
+      const done = e.target.checked;
+      return {
+        ...task,
+        done,
+        finishDate: done ? new Date().toISOString() : null,
+      };
+    }
+    return task;
+  });
+  setItem('tasksList', newTasksList);
+  renderTasks();
+};

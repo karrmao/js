7cc5ace0c4887a01af1694bd92d7220d64ecdf4d
karrmao <karrmao@gmail.com>
2022-09-03 14:28:38 +0300

    add

--- lesson_27_browser_(local)_storage/task3/src/scripts/updateTask.js
+++ lesson_27_browser_(local)_storage/task3/src/scripts/updateTask.js
@@ -1,36 +1,16 @@
 import { renderTasks } from './rendered.js';
 import { getItem, setItem } from './storage.js';
 
-export const onToggleTask = () => {
-  const taskTitleInputElem = document.querySelector('.task-input');
-  const text = taskTitleInputElem.value;
-  if (!text) {
+export const onToggleTask = (event) => {
+  const isCheckbox = event.target.classList.contains('list__item-checkbox');
+
+  if (!isCheckbox) {
     return;
   }
-  taskTitleInputElem.value = '';
-  const tasksList = getItem('tasksList') || [];
-  const newTasksList = tasksList.concat({
-    text,
-    done: false,
-    createDate: new Date().toISOString(),
-    id: Math.random().toString(),
-  });
-  setItem('tasksList', newTasksList);
-
-  renderTasks();
-};
-
-////
-export const onToggleTask = (e) => {
-  const isCheckBox = e.target.classList.contains('list__item-checkbox');
-
-  if (!isCheckBox) {
-    return;
-  }
-  const taskslist = getItem('tasksList');
-  const newTasksList = taskslist.map((task) => {
-    if (task.id === e.target.dataset.id) {
-      const done = e.target.checked;
+  const tasksList = getItem('tasksList');
+  const newTasksList = tasksList.map((task) => {
+    if (task.id === event.target.dataset.id) {
+      const done = event.target.checked;
       return {
         ...task,
         done,

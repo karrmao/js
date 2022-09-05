59af9d7a0e04542e4e7eb4abd6aabea75c760aa8
karrmao <karrmao@gmail.com>
2022-09-05 20:58:06 +0300

    add

---
+++ lesson_29_asynchronous/task1/index.js
@@ -0,0 +1,34 @@
+const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Photo');
+
+  imgElem.src = imgSrc;
+
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  /**********************************************/
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImageLoaded);
+  /**********************************************/
+  imgElem.addEventListener('error', () => callback('Image load failed'));
+};
+
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+addImage(imgSrc, onImageLoaded);

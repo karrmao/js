bd42fb6f81d84894f98f5faccd71d6ad006a6e9b
karrmao <karrmao@gmail.com>
2022-09-05 12:01:04 +0300

    add

--- lesson_28_func_advamced/task4/index.js
+++ lesson_28_func_advamced/task4/index.js
@@ -3,11 +3,11 @@ const mult2 = (value) => value * 2; //6 * 2 = 12
 const div4 = (value) => value / 4; //12 / 4 = 3
 const mult10 = (value) => value * 10; //3 * 10 = 30
 
-export const comprose =
+export const compose =
   (...funcs) =>
   (value) => {
     return funcs.reduce((acc, func) => func(acc), value);
   };
 
-const doEverithing = comprose(add3, mult2, div4, mult10);
+const doEverithing = compose(add3, mult2, div4, mult10);
 console.log(doEverithing(3)); //=> 30

993aa93140bfe87d506448cba1f0ba1a69b338e0
karrmao <karrmao@gmail.com>
2022-09-06 21:39:12 +0300

    add

--- lesson_31_promise_handlers_chaining/task2/index.js
+++ lesson_31_promise_handlers_chaining/task2/index.js
@@ -1,3 +1,32 @@
+// const asyncCalculator = (num) =>
+//   new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(`Initial value: ${num}`);
+//       resolve(num);
+//     }, 300);
+//   }).then(
+//     (value) =>
+//       new Promise((resolve) => {
+//         setTimeout(() => {
+//           const result = value * value;
+//           console.log(`Squared value: ${result}`);
+//           resolve(result);
+//         }, 300);
+//       })
+//   );
+
+// const p = asyncCalculator(4)
+//   .then((value) => {
+//     const result = value * 2;
+//     console.log(`Doubled value: ${result}`);
+//     return result;
+//   })
+//   .then((value) => console.log(value));
+// //=> Initial value: 4
+// //=> Squared value: 16
+// //=> Doubled value: 32
+// //=> 32
+/******************************************/
 export const asyncCalculator = (num) =>
   new Promise((resolve) => {
     setTimeout(() => {
@@ -26,6 +55,7 @@ export const asyncCalculator = (num) =>
         })
     );
 
+//-- test data
 asyncCalculator(5);
 //=> Initial value: 5
 //=> Squared value: 25

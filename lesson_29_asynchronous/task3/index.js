c32dd123e9f27d3bf49a8b4c12842eb996152aec
karrmao <karrmao@gmail.com>
2022-09-06 11:03:01 +0300

    add

---
+++ lesson_29_asynchronous/task3/index.js
@@ -0,0 +1,31 @@
+// const main = () => {
+//   console.log('1');
+//   setTimeout(() => console.log('2'), 1000);
+//   console.log('3');
+//   setTimeout(() => console.log('4'), 0);
+//   console.log('5');
+// };
+
+// main();
+
+export const printing = () => {
+  console.log(1);
+
+  setTimeout(function () {
+    console.log(5);
+  }, 1000);
+
+  console.log(2);
+
+  setTimeout(function () {
+    console.log(4);
+  }, 0);
+
+  setTimeout(function () {
+    console.log(6);
+  }, 2000);
+
+  console.log(3);
+};
+
+printing();

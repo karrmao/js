4764f8c6c668c5507dc5d7882bab12c291f2054c
karrmao <karrmao@gmail.com>
2022-09-07 13:39:49 +0300

    add

--- lesson_32_promise_static_methods/task1/index.js
+++ lesson_32_promise_static_methods/task1/index.js
@@ -5,16 +5,17 @@ const getWalueWithDelay = (value, delay) =>
       resolve(value);
     }, delay);
   });
+
 const asyncNumber1 = getWalueWithDelay(56, 1000);
 const asyncNumber2 = getWalueWithDelay(undefined, 2000);
 const asyncNumber3 = getWalueWithDelay('10', 1000);
 
-export const getSum = (numbers) =>
+const getSum = (numbers) =>
   numbers
     .filter((value) => !isNaN(value))
     .feduce((acc, num) => acc + Number(num), 0);
 
-const asyncSum = (...asyncNumbers) => {
+export const asyncSum = (...asyncNumbers) => {
   return Promise.all(asyncNumbers)
     .then((numbers) => getSum(numbers))
     .catch(() => Promise.reject(new Error(`Can't calculate`)));

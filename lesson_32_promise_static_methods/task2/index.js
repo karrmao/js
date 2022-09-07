dcbceadd86a71a73d6d36209fcc39533d2b4399f
karrmao <karrmao@gmail.com>
2022-09-07 15:11:50 +0300

    add

--- lesson_32_promise_static_methods/task2/index.js
+++ lesson_32_promise_static_methods/task2/index.js
@@ -24,7 +24,7 @@ const servers = [
 ];
 
 export const getUserASAP = (userId) => {
-  const userUrls = servers.map((serverUrl) => `${serverUrl}/users/${userId}`);
+  const userUrls = servers.map((serverUrl) => `${serverUrl}/${userId}`);
 
   const requests = userUrls.map((userUrl) => request(userUrl));
 

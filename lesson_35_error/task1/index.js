9e0617987cb792812492b5292db87717ddc2078f
karrmao <karrmao@gmail.com>
2022-09-13 18:28:23 +0300

    add

--- lesson_35_error/task1/index.js
+++ lesson_35_error/task1/index.js
@@ -17,7 +17,7 @@ try {
 let productParsingResult;
 
 try {
-  const product = JSON.parse('{ "name": "Bob" }');
+  const product = JSON.parse('{ "name": "Bob }');
   console.log('Product data: ', product);
   productParsingResult = 'success';
 } catch (e) {

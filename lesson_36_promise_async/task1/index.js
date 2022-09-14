889e5c5b1e13a5b24f03d14a974868cfaba0b2db
karrmao <karrmao@gmail.com>
2022-09-14 18:47:53 +0300

    add

---
+++ lesson_36_promise_async/task1/index.js
@@ -0,0 +1,18 @@
+//'use strict';
+
+export const fetchUser = async (userId) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    if (!response.ok) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to fetch user');
+  }
+};
+
+fetchUser('facebook')
+  .then((userData) => console.log(userData))
+  .catch((err) => alert(err.message));

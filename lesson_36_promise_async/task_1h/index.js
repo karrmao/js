a24a1b2dc4c1b125f63720ecc61ab9393ff1eeb2
karrmao <karrmao@gmail.com>
2022-09-15 16:29:21 +0300

    add

--- lesson_36_promise_async/task_1h/index.js
+++ lesson_36_promise_async/task_1h/index.js
@@ -7,17 +7,15 @@ const getUsersBlogs = async (users) => {
         (response) => response.json()
       );
     });
-    console.log(usersArr);
     const resultArr = await Promise.all(usersArr);
-    console.log(resultArr);
     return resultArr.map(({ blog }) => blog);
   } catch (error) {
     throw new Error('Failed to load user data');
   }
 };
 
-// examples
-getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
-  console.log(linksList)
-); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// // examples
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
+//   console.log(linksList)
+// ); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
